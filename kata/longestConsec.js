ea0e422b434acf4e7764fc83cbf70b532571c22c
Ebuka Hills <flyg101@gmail.com>
2017-09-27 09:52:41 +0100

    solve longestConsec

---
+++ kata/longestConsec.js
@@ -0,0 +1,43 @@
+// https://www.codewars.com/kata/consecutive-strings/train/javascript
+
+/**
+ * 
+ * You are given an array strarr of strings and an integer k.
+ * Your task is to return the first longest string consisting of k consecutive strings taken in the array.
+ * #Example: longest_consec(["zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail"], 2) --> "abigailtheta"
+ * n being the length of the string array, if n = 0 or k > n or k <= 0 return "".
+ */
+
+function longestConsec(strarr, k) {
+  // your code
+
+  if (k <= 0 || k > strarr.length) return '';
+
+  let start = strarr.map((a, i) => i).reduce((a, b) => {
+    return strarr.slice(a, a + k).join('').length > strarr.slice(b, b + k).join('').length ? a : b;
+  }, 0);
+
+  return strarr.slice(start, start + k).join('');
+}
+
+/**
+ * TESTS
+ * 
+ * function testing(actual, expected) {
+    Test.assertEquals(actual, expected)
+}
+
+Test.describe("longestConsec",function() {
+Test.it("Basic tests",function() { 
+    testing(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2), "abigailtheta")
+    testing(longestConsec(["ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"], 1), "oocccffuucccjjjkkkjyyyeehh")
+    testing(longestConsec([], 3), "")
+    testing(longestConsec(["itvayloxrp","wkppqsztdkmvcuwvereiupccauycnjutlv","vweqilsfytihvrzlaodfixoyxvyuyvgpck"], 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck")
+    testing(longestConsec(["wlwsasphmxx","owiaxujylentrklctozmymu","wpgozvxxiu"], 2), "wlwsasphmxxowiaxujylentrklctozmymu")
+    testing(longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], -2), "")
+    testing(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 3), "ixoyx3452zzzzzzzzzzzz")
+    testing(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 15), "")
+    testing(longestConsec(["it","wkppv","ixoyx", "3452", "zzzzzzzzzzzz"], 0), "")
+})})
+
+ */
